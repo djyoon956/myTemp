c1101628c59f93e730570d92a60bd49f3eac089c
dajeong.yoon <djyoon956@gmail.com>
2019-12-12 20:39:17 +0900

    add

---
+++ MvnProject/javaproject/src/main/java/bit/board/App.java
@@ -0,0 +1,13 @@
+package bit.board;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
